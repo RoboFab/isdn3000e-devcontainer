f053cb8f5b0242bb50a46a6ccc23534409e533b6
Xingxin HE <xingxin.he@mail.polimi.it>
2026-04-06 12:51:32 +0800

    feat: migrate missing dependencies and post-create hook from legacy devcontainer

---
+++ post-create.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+source /opt/ros/humble/setup.bash
+set -u
+
+FRANKA_DESCRIPTION_DIR="/workspace/src/franka_description"
+FRANKA_DESCRIPTION_VERSION="1.3.0"
+FRANKA_DESCRIPTION_REPO="https://github.com/frankarobotics/franka_description.git"
+
+if [ ! -d "$FRANKA_DESCRIPTION_DIR/.git" ]; then
+  git clone --branch "$FRANKA_DESCRIPTION_VERSION" --depth 1 \
+    "$FRANKA_DESCRIPTION_REPO" "$FRANKA_DESCRIPTION_DIR"
+else
+  git -C "$FRANKA_DESCRIPTION_DIR" fetch --tags origin
+  git -C "$FRANKA_DESCRIPTION_DIR" checkout "$FRANKA_DESCRIPTION_VERSION"
+fi
+
+sudo rosdep install --from-paths src --ignore-src --rosdistro humble -r -y
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
