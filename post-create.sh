0fd88a9af0bd7c07cc73460e69ff62a19bbd49c5
Xingxin HE <xingxin.he@mail.polimi.it>
2026-04-20 13:46:55 +0800

    feat: gazebo

--- post-create.sh
+++ post-create.sh
@@ -17,4 +17,11 @@ else
 fi
 
 sudo rosdep install --from-paths src --ignore-src --rosdistro humble -r -y
+
+# The workspace is bind-mounted and may contain stale colcon artifacts from
+# another host/container path. Clean once to avoid symlink-install conflicts.
+if [ -d /workspace/build ] || [ -d /workspace/install ] || [ -d /workspace/log ]; then
+  rm -rf /workspace/build /workspace/install /workspace/log
+fi
+
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
