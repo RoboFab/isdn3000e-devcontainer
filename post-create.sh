d3b75db82b0fc29f0e2442610e7e4525a68c05b7
Xingxin HE <xingxin.he@connect.ust.hk>
2026-04-21 05:30:12 +0000

    feat: viser port

--- post-create.sh
+++ post-create.sh
@@ -4,18 +4,6 @@ set -eo pipefail
 source /opt/ros/humble/setup.bash
 set -u
 
-FRANKA_DESCRIPTION_DIR="/workspace/src/franka_description"
-FRANKA_DESCRIPTION_VERSION="1.3.0"
-FRANKA_DESCRIPTION_REPO="https://github.com/frankarobotics/franka_description.git"
-
-if [ ! -d "$FRANKA_DESCRIPTION_DIR/.git" ]; then
-  git clone --branch "$FRANKA_DESCRIPTION_VERSION" --depth 1 \
-    "$FRANKA_DESCRIPTION_REPO" "$FRANKA_DESCRIPTION_DIR"
-else
-  git -C "$FRANKA_DESCRIPTION_DIR" fetch --tags origin
-  git -C "$FRANKA_DESCRIPTION_DIR" checkout "$FRANKA_DESCRIPTION_VERSION"
-fi
-
 sudo rosdep install --from-paths src --ignore-src --rosdistro humble -r -y
 
 # The workspace is bind-mounted and may contain stale colcon artifacts from
